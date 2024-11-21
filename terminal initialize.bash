25480f1d5cea418fb4756bfe26a2e1630bde082e
RondoDuckFish <95487783+RondoDuckFish@users.noreply.github.com>
2024-11-21 14:37:39 -0500

    made terminal initialize.bash
    made this

---
+++ terminal initialize.bash
@@ -0,0 +1,11 @@
+source venv/bin/activate
+
+#this makes a virtual venv environment bin which is activated
+#run each line of this secrion SEPERATLEY
+
+pip install pandas numpy scikit-learn nltk
+
+#Running this command installs these libraries into your Python environment, enabling you to use them in your machine learning project.
+
+python SpamMailDetector.py
+# you run this one last after you have all of the code on the computer run thru plz do this as a last step
